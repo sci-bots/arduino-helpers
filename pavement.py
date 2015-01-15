0e54c64814d42fec62623b552c9cfcab3f9ec423
Ryan Fobel <ryan@fobel.net>
2015-01-15 12:20:16 -0500

    Move import of finddata so that we use local copy

--- pavement.py
+++ pavement.py
@@ -1,14 +1,14 @@
 from paver.easy import task, needs
 from paver.setuputils import setup
-from finddata import find_package_data
 
 import os
 import sys
 
 # add the current directory as the first listing on the python path
-# so that we import the correct version.py
+# so that we import the correct version.py and finddata.py
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 import version
+from finddata import find_package_data
 
 
 setup(name='arduino_helpers',
